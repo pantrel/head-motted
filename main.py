69914aa8f584abcdfedd20a66bebebaef8b07c02
Liis-Marii Küüner <liismarii8@gmail.com>
2017-12-19 07:15:19 +0200

    Põhiplaan

--- main.py
+++ main.py
@@ -3,26 +3,51 @@ def clear():
     import os
     os.system("cls" if os == "nt" else "clear")
 
-
-def kuva():
-    pass
-
-def lisa():
-    pass
-
-while True:
-
     print("Mõtte kuvamiseks vajuta 'Enter'.")
+    print("Ekraani puhastamiseks vajuta 'c'.")
     print("Mõtte lisamiseks vajuta 'a'.")
     print("Väljumiseks vajuta 'e'.")
-
+#Defineerin programmist väljumise:
+def exit():
+    import sys
+    import os
+    os.system("cls" if os == "nt" else "clear")
+    sys.exit()
+#Defineerin suvalise mõtte kuvamise:
+def kuva():
+    import random
+    with open("Aforismid","r") as fail:
+        sisu = fail.readlines()
+        print(random.choice(sisu))
+#Defineerin mõtte lisamise:
+def lisa():
+    with open("Aforismid", "a") as fail:
+        mote = input("Lisa mõte:\n> ")
+        print("Kas soovid mõtte salvestada? y/n")
+        kinnitus = input("> ")
+        if kinnitus == "y" or kinnitus == "Y":
+            print(mote)
+            fail.write(str(mote+"\n"))
+            print("[+]")
+            main()
+        else:
+            clear()
+            main()
+#Defineerin programmi tuuma:
+def main():
     valik = input("> ")
 
-    if valik == "e" or "E":
-        import sys
+    if valik == "e" or valik == "E":
+        exit()
+    elif valik == "a" or valik == "A":
+        lisa()
+        main()
+    elif valik == "":
+        kuva()
+        main()
+    elif valik == "c" or valik == "C":
         clear()
-        sys.exit()
-    elif valik == "a" or "A":
-        pass
-    else:
-        pass
+        main()
+
+clear()
+main()
