a498116aca2a2e21eedab52743e7640427ea9dcc
Liis-Marii Küüner <liismarii8@gmail.com>
2017-12-21 04:37:39 +0200

    Changes to be committed: 	modified:   Aforismid 	modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # head-motted
 Heade mõtete koda.
+
+Tegemist on programmiga, mis kuvab kasutajale häid mõtteid, mõtteteri, mõtisklusi jms. Mõtted on valitud Heiki ja Ringa Raudla koostatud ja tõlgitud "Aforismide raudvara"st alustades esimesest leheküljest. Aforisme on valitud igalt leheküljelt selles raamatus.
+
+Liis-Marii K
--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 def clear():
     import os
     os.system("cls" if os == "nt" else "clear")
-
-    print("Mõtte kuvamiseks vajuta 'Enter'.")
-    print("Ekraani puhastamiseks vajuta 'c'.")
-    print("Mõtte lisamiseks vajuta 'a'.")
-    print("Väljumiseks vajuta 'e'.")
+def tervitus():
+    print("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---")
+    print("Oled kasutamas heade mõtete kuvamise programmi!")
+    print("Suvalise mõtte kuvamiseks vajuta 'Enter'.")
+    print("Ekraani puhastamiseks sisesta 'c'.")
+    print("Mõtte lisamiseks sisesta 'a'.")
+    print("Väljumiseks sisesta 'e'.")
 #Defineerin programmist väljumise:
 def exit():
     import sys
@@ -17,29 +19,34 @@ def exit():
 def kuva():
     import random
     with open("Aforismid","r") as fail:
-        sisu = fail.readlines()
-        print(random.choice(sisu))
+        sisu = fail.read().splitlines()
+        print(random.choice(sisu)+"\n")
+
 #Defineerin mõtte lisamise:
 def lisa():
     with open("Aforismid", "a") as fail:
-        mote = input("Lisa mõte:\n> ")
+        mote = input("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---\nSisesta mõte:\n> ")
         print("Kas soovid mõtte salvestada? y/n")
         kinnitus = input("> ")
+        print(mote)
         if kinnitus == "y" or kinnitus == "Y":
-            print(mote)
             fail.write(str(mote+"\n"))
-            print("[+]")
+            clear()
+            tervitus()
             main()
         else:
             clear()
+            tervitus()
             main()
 #Defineerin programmi tuuma:
 def main():
-    valik = input("> ")
+    valik = input("---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---\n")
 
     if valik == "e" or valik == "E":
         exit()
     elif valik == "a" or valik == "A":
+        clear()
+        tervitus()
         lisa()
         main()
     elif valik == "":
@@ -47,7 +54,9 @@ def main():
         main()
     elif valik == "c" or valik == "C":
         clear()
+        tervitus()
         main()
 
 clear()
+tervitus()
 main()
